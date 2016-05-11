2b04d3b60001e540ebf89a6aad96d2bd0aed8c11
davidarangomedina <david_arango82111@elpoli.edu.co>
2016-05-11 06:08:46 -0500

    Cambio de colores con el contador
    Cada vez que se inicia el contador, la palabra que contiene el color
    cambia el texto y su color, además visualiza si la opción que el usuario
    escoja es correcta o incorrecta

--- strooperApp/app/src/main/java/com/example/openlab/strooperapp/StrooperActivity.java
+++ strooperApp/app/src/main/java/com/example/openlab/strooperapp/StrooperActivity.java
@@ -6,13 +6,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class StrooperActivity extends AppCompatActivity {
 
-    TextView txtSegundos,txtNick,txtPuntaje;
+    TextView txtSegundos,txtNick,txtPuntaje, txtColor;
     EditText nick2;
+    String vector_colores[]={"Azul","Verde","Rojo","Amarillo"}, colorPalabra="";
 
     int correctos=0,intentos=0;
 
@@ -25,10 +28,29 @@ public class StrooperActivity extends AppCompatActivity {
 
     }
 
+    public void BotonTrue(View view){
+        if(txtColor.getText().equals(colorPalabra)){
+            Toast.makeText(getBaseContext(), "Correcto", Toast.LENGTH_SHORT).show();
+            correctos++;
+        }else{
+            Toast.makeText(getBaseContext(), "Incorrecto", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void BotonFalse(View view){
+        if(!txtColor.getText().equals(colorPalabra)){
+            Toast.makeText(getBaseContext(), "Correcto", Toast.LENGTH_SHORT).show();
+            correctos++;
+        }else{
+            Toast.makeText(getBaseContext(), "Incorrecto", Toast.LENGTH_SHORT).show();
+        }
+    }
+
     private void inicializarCampos(){
         txtNick = (TextView) findViewById(R.id.txtNick);
         txtPuntaje = (TextView) findViewById(R.id.txtPuntaje);
         txtSegundos = (TextView) findViewById(R.id.txtSegundos);
+        txtColor = (TextView) findViewById(R.id.txtColor);
 
        Bundle bundle=getIntent().getExtras();
         if(bundle!=null){
@@ -42,6 +64,8 @@ public class StrooperActivity extends AppCompatActivity {
 
     public void jugarAhora(View view){
         if(txtSegundos.getText().toString().equalsIgnoreCase("Jugar Ahora")){
+            cambiarColorTexto(txtColor);
+            cambiarTexto(txtColor);
             inicarCuentaAtras();
         }
     }
@@ -58,16 +82,48 @@ public class StrooperActivity extends AppCompatActivity {
             public void onTick(long miliSegundos) {
                 segundos = (int) (miliSegundos / 1000);
                 txtSegundos.setText("" + (segundos - 1));
+                //Modificado por David A.
+                if(txtSegundos.getText().equals("0")){
+                    //cambiarColorTexto(txtColor);
+                    //cambiarTexto(txtColor);
+                }
             }
 
             @Override
             public void onFinish() {
+                cambiarColorTexto(txtColor);
+                cambiarTexto(txtColor);
                 inicarCuentaAtras();
                 intentos++;
             }
         };
 
         counter.start();
+    }
+
+    //Creado por David A.
+    public void cambiarColorTexto(TextView colortexto){
+        int max=4;
+        switch (new Random().nextInt(max)){
+            case 0: txtColor.setTextColor(getResources().getColor(R.color.verde));
+                colorPalabra="Verde";
+                break;
+            case 1: txtColor.setTextColor(getResources().getColor(R.color.azul));
+                colorPalabra="Azul";
+                break;
+            case 2: txtColor.setTextColor(getResources().getColor(R.color.amarillo));
+                colorPalabra="Amarillo";
+                break;
+            case 3: txtColor.setTextColor(getResources().getColor(R.color.rojo));
+                colorPalabra="Rojo";
+                break;
+        }
+
+    }
+
+    //Creado por David A.
+    public void cambiarTexto(TextView texto){
+        texto.setText(vector_colores[new Random().nextInt(4)]);
     }
 }
   /*  public class CounterClass extends CountDownTimer {
